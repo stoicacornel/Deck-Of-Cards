6d579720cb22fea831f619e465484cbb9bf0e89a
Aelfric <StoicaCornel749@yahoo.com>
2019-01-11 12:51:31 +0000

    Initial commit. First C# Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deck_Of_Cards
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
